74854d71c5357b71e28b60eabb59fbab60d3f8bb
秋井乃音斗 <archy1none@gmail.com>
2025-09-24 14:43:25 +0900

    Update example.rs

--- example.rs
+++ example.rs
@@ -1,22 +1,7 @@
 fn main() {
 	let r#_a = String::from("Hello");
 	{
-		let r#_b = r#_a.clone();
-		{
-			let r#_c = r#_b.clone();
-			{
-				let r#_d = r#_b;
-				{
-					let r#_a = r#_d;
-					{
-						let r#_d = r#_a.clone();
-						{
-							let r#_c = r#_a;
-							r#_c;
-						};
-					};
-				};
-			};
-		};
+		let r#_b = r#_a;
+		r#_b;
 	};
 }
