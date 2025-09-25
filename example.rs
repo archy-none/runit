b892068679380a570bde9e62ce3f9ff06fa42e74
秋井乃音斗 <archy1none@gmail.com>
2025-09-25 17:57:48 +0900

    Update example.rs

--- example.rs
+++ example.rs
@@ -1,11 +1,11 @@
 fn main() {
-	let mut r#_a = String::from("Hello") + &String::from("Be");
+	let mut r#_a = String::from("Hello") + &String::from("World");
 	{
-		let r#_b = 100isize + 2isize;
+		let r#_b = 10isize / 2isize;
 		{
-			r#_a = String::from("Bye");
+			r#_a = r#_a.clone().repeat(r#_b);
 			{
-				let r#_c = r#_a.clone();
+				let r#_c = r#_a;
 				r#_a;
 			};
 		};
