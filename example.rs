7ded67c8675a6616344f3272dd5812df00c1926f
秋井乃音斗 <archy1none@gmail.com>
2025-09-25 23:33:06 +0900

    Update example.rs

--- example.rs
+++ example.rs
@@ -1,9 +1,9 @@
 fn main() {
 	println!("{}", {
-		fn r#_inc(r#_n: isize) -> isize {
-			r#_n + 1isize
+		fn r#_inc(r#_a: isize, r#_b: isize) -> isize {
+			r#_a + r#_b
 		}
-		r#_inc(5isize, 4isize)
+		r#_inc(5isize, 3isize)
 	});
 }
 
