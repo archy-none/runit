dc6d8501ef53018b66b7374d9e12bedbe4fc3d34
秋井乃音斗 <archy1none@gmail.com>
2025-09-26 01:21:58 +0900

    Update example.rs

--- example.rs
+++ example.rs
@@ -2,9 +2,13 @@ fn main() {
 	println!("{}", {
 		fn r#_add(r#_a: isize, r#_b: isize) -> isize {
 			let r#_c = r#_a;
-			r#_a + r#_c
+			r#_b + r#_c
 		}
-		r#_add(5isize + 2isize, r#_add(3isize, 3isize))
+		let r#_n = r#_add(5isize + 2isize, r#_add(3isize, 3isize));
+		let mut r#_c = String::from("Hello! ");
+		let r#_d = r#_c.clone();
+		r#_c = r#_c.clone().repeat(r#_n as usize);
+		r#_c
 	});
 }
 
