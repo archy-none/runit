46faebcf2200bf7b1c969dfd7f30f2978188b13c
秋井乃音斗 <archy1none@gmail.com>
2025-09-26 20:46:37 +0900

    Update example.rs

--- example.rs
+++ example.rs
@@ -7,9 +7,9 @@ fn main() {
 		fn r#_main(r#_a: String, r#_b: isize) -> String {
 			let r#_n = r#_add(1isize + 2isize, r#_add(r#_b, -3isize));
 			let r#_c = String::from("Hello, ");
-			let r#_d = r#_c.clone() + &r#_a + &String::from("! ");
-			let r#_e = r#_c + &r#_d.clone();
-			r#_e + &r#_d.repeat(r#_n as usize)
+			let r#_d = [[r#_c.clone(), r#_a].concat(), String::from("! ")].concat();
+			let r#_e = [r#_c, r#_d.clone()].concat();
+			[r#_e, r#_d].concat().repeat(r#_n as usize)
 		}
 		r#_main(String::from("world"), 3isize)
 	});
